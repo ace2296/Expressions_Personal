4392771d1e10687c6bca6c9d47596addbbf0dec8
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 01:02:08 -0800

    Added month by month variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 //Guerra, Andre 12/13/14 Expressions - Personal
 
 var month = ["May" , "June", "July" , "August"]; //Summer months are listed
+
+var month1 = prompt("Let's find out how much money you saved this summer n/How much money did you save in the month of"+ month[0] + "?");
+var month2 = prompt("How much money did you save in the month of"+ month[1] + "?");
+var month3 = prompt("How much money did you save in the month of"+ month[2] + "?");
+var month4 = prompt("How much money did you save in the month of"+ month[3] + "?");
+
+var total = month1 + month2 + month3 + month4;
+
+
+
+
