2c49fa82fab97cc5320bb26040280073e6db7562
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 01:14:41 -0800

    Converted string inputs into numbers.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 
 var month = ["May" , "June", "July" , "August"]; //Summer months are listed
 
-var month1 = prompt("Let's find out how much money you saved this summer n/How much money did you save in the month of"+ month[0] + "?");
-var month2 = prompt("How much money did you save in the month of"+ month[1] + "?");
-var month3 = prompt("How much money did you save in the month of"+ month[2] + "?");
-var month4 = prompt("How much money did you save in the month of"+ month[3] + "?");
+var month1 = prompt("Let's find out how much money you saved this summer. How much money did you save in the month of "+ month[0] + "?");
+var month2 = prompt("How much money did you save in the month of "+ month[1] + "?");
+var month3 = prompt("How much money did you save in the month of "+ month[2] + "?");
+var month4 = prompt("How much money did you save in the month of "+ month[3] + "?");
 
-var total = month1 + month2 + month3 + month4;
+var total = Number(month1) + Number(month2) + Number(month3) + Number(month4);
 var monthAverage = total / 4;
 var weekAverage = total / 16;
 
-var saved = alert("You save a total of $" + total + " this summer with an avergae savings of $" + monthAverage + " saved per month and an average of $" + weekAverage + "saved per week.");
+alert("You save a total of $" + total + " this summer with an avergae savings of $" + monthAverage + " saved per month and an average of $" + weekAverage + "saved per week.");
 
 
