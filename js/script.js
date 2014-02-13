4763633df3c743b2685515b5d5d0c3ca8b33db73
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-13 01:20:45 -0800

    Added comments and errors in final alert.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
 
 var month = ["May" , "June", "July" , "August"]; //Summer months are listed
 
-var month1 = prompt("Let's find out how much money you saved this summer. How much money did you save in the month of "+ month[0] + "?");
-var month2 = prompt("How much money did you save in the month of "+ month[1] + "?");
-var month3 = prompt("How much money did you save in the month of "+ month[2] + "?");
-var month4 = prompt("How much money did you save in the month of "+ month[3] + "?");
+var month1 = prompt("Let's find out how much money you saved this summer. How much money did you save in the month of "+ month[0] + "?"); //gather info for May
+var month2 = prompt("How much money did you save in the month of "+ month[1] + "?"); //gather info for June
+var month3 = prompt("How much money did you save in the month of "+ month[2] + "?"); //gather info for July
+var month4 = prompt("How much money did you save in the month of "+ month[3] + "?"); //gather info for August
 
-var total = Number(month1) + Number(month2) + Number(month3) + Number(month4);
-var monthAverage = total / 4;
-var weekAverage = total / 16;
+var total = Number(month1) + Number(month2) + Number(month3) + Number(month4); //add up the user input values and convert them into numbers
+var monthAverage = total / 4; //divided by the total number of months in the array
+var weekAverage = total / 16; //divided by the total number of weeks in the 4 months
 
-alert("You save a total of $" + total + " this summer with an avergae savings of $" + monthAverage + " saved per month and an average of $" + weekAverage + "saved per week.");
+//This alert will tell the user how much they have saved up over the summer and what was their average savings per month and per week.
+
+alert("Congratulations! \nYou save a total of $" + total + " this summer, with an average savings of $" + monthAverage + " saved per month and an average of $" + weekAverage + " saved per week.");
 
 
